62b9e3aa1de151fb9affc580e73973c64beee261
jeffgurd888 <jeffgurd888@gmail.com>
2026-08-05 17:05:07 -0400

    Add lakefile: require latest Mathlib4

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package unity_theory
+
+require mathlib4 from git "https://github.com/leanprover-community/mathlib4" @ "main"
+
+@[default_target]
+lean_lib UnityTheory
